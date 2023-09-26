2ece75d0a9e9dc13f5779c9b439429645cd4a9b5
Gyeongrak <peter6617@gmail.com>
2023-09-26 18:19:39 +0900

    나눗셈 연산자 응용하여 수식도출하기

---
+++ DivisionAndIntegers2.java
@@ -0,0 +1,11 @@
+package work;
+
+public class DivisionAndIntegers2  {
+    public static void main(String[] args) {
+        int num = 333;
+        System.out.println(num/10*10+1);
+
+        /*나눗셈연산자는 반올림하지 않고 버림을 하는 성질을 이용
+        333/10 = 33 => 33*10 => 330 => 330+1 = 331*/
+    }
+}
