eab343d93af7d6bf588c2fd774b942fe2de57dc1
Gyeongrak <peter6617@gmail.com>
2023-09-26 18:16:21 +0900

    나눗셈연산자는 버림을 한다는 성질을 이용한 계산수식

---
+++ DivisionAndIntegers.java
@@ -0,0 +1,12 @@
+package work;
+
+public class DivisionAndIntegers {
+    public static void main(String[] args) {
+        int num = 456;
+        System.out.println(num/100*100);
+    /*나눗셈연산자는 반올림하지 않고 버리는 성질을 이용함
+    456/100 = 4 => 4*100 = 400*/
+    }
+
+}
+
