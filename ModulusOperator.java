a921718bddaa631c17818d56048bf4b33681ac9b
Gyeongrak <peter6617@gmail.com>
2023-09-26 18:28:09 +0900

    나머지연산자를 응용한 계산 및 수식 도출하기

---
+++ ModulusOperator.java
@@ -0,0 +1,10 @@
+package work;
+
+public class ModulusOperator {
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(10 - num%10);
+        //나머지연산자를 이용한 계산
+        //다른 정답으로 '(num/10+1)* -num'
+    }
+}
